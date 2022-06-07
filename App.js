d30e503d0cbf43ac3bb4fe31f9a63c798110fc93
YJ Soon <yjsoon@tinkertanker.com>
2022-06-07 14:43:10 +0800

    Useless optimisations

--- App.js
+++ App.js
@@ -1,46 +1,40 @@
 import React, { useState } from "react";
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
-import BlockRGB from './components/BlockRGB';
+import BlockRGB from "./components/BlockRGB";
 import { FlatList } from "react-native-gesture-handler";
 
-
 function HomeScreen() {
-
-  const [colorArray, setColorArray] = useState([
-    { red: 255, green: 0, blue: 0, id: "0" },
-    { red: 0, green: 255, blue: 0, id: "1" },
-    { red: 0, green: 0, blue: 255, id: "2" },
-  ])
+  const [colorArray, setColorArray] = useState([]);
 
   function renderItem({ item }) {
-    return <BlockRGB red={item.red} green={item.green} blue={item.blue} />
+    return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
   }
 
   function addColor() {
     setColorArray([
-      ...colorArray,
       {
         red: Math.floor(Math.random() * 256),
         green: Math.floor(Math.random() * 256),
         blue: Math.floor(Math.random() * 256),
-        id: `${colorArray.length}`
-      }
-    ])
+        id: `${colorArray.length}`,
+      },
+      ...colorArray,
+    ]);
+    console.log(colorArray);
   }
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={{ height: 40, justifyContent: 'center' }} onPress={addColor}>
+      <TouchableOpacity
+        style={{ height: 40, justifyContent: "center" }}
+        onPress={addColor}
+      >
         <Text>Add colour</Text>
       </TouchableOpacity>
-      <FlatList
-        style={styles.list}
-        data={colorArray}
-        renderItem={renderItem}
-      />
+      <FlatList style={styles.list} data={colorArray} renderItem={renderItem} />
     </View>
   );
 }
@@ -54,12 +48,12 @@ export default function App() {
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
 });
